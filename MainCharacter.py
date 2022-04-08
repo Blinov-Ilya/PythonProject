3c804797440302215fef2ac03ebd584ff618433a
Blinov-Ilya <93616448+Blinov-Ilya@users.noreply.github.com>
2022-04-08 23:52:43 +0300

    Create MainCharacter.py

---
+++ MainCharacter.py
@@ -0,0 +1,26 @@
+from Loot import MedicineGenerator
+from Loot import ArmorGenerator
+from Loot import WeaponGenerator
+
+
+class MainCharacter:
+    name, hp, hunger, thirst, cheerfulness, radiation, inventory_set, \
+        observation, stealthiness, current_weapon, head_armor, body_armor = \
+        "", 0, 0, 0, 0, 0, set(), 0, 0, WeaponGenerator.generate_knife(), ArmorGenerator.generate_motorcycle_helmet(), \
+        ArmorGenerator.generate_motorcycle_jacket()
+
+    def __init__(self, name, hp, hunger, thirst, cheerfulness, radiation, inventory_set,
+                 observation, stealthiness, current_weapon, head_armor, body_armor):
+        self.name = name
+        self.hp = hp
+        self.hunger = hunger
+        self.thirst = thirst
+        self.cheerfulness = cheerfulness
+        self.radiation = radiation
+        self.inventory_set = inventory_set
+        self.observation = observation
+        self.stealthiness = stealthiness
+        self.current_weapon = current_weapon
+        self.head_armor = head_armor
+        self.body_armor = body_armor
+
