357cf487a22e9d108c813b122bdb009a422cc28d
Blinov-Ilya <blinov.is@phystech.edu>
2022-04-10 18:04:15 +0300

    Changing Event loop

--- EventLoop.py
+++ EventLoop.py
@@ -3,105 +3,96 @@ import Mobs
 import Loot
 import Locations
 
+
 # recursive function?
 
 
-class Replica:
+class Replica:  # replica determines by the text_of_choice_before?
+    # main_action is a function of changes takes the current condition
 
-    def __init__(self, text_of_choice_before, text, check_if_applicable, main_action, list_of_replicas_next):
+    def __init__(self, text_of_choice_before, text, check_if_applicable, main_action, list_of_answers,
+                 map_answer_hash_replica):
         self.text_of_choice_before = text_of_choice_before
         self.text = text
         self.check_if_applicable = check_if_applicable
         self.main_action = main_action
-        self.list_of_replicas_next = list_of_replicas_next
+        self.list_of_answers = list_of_answers
+        self.map_answer_hash_replica = map_answer_hash_replica
 
 
-class MainStatChange:
+# class MainStatChange:
+#
+#     def __init__(self, hp_change, hunger_change, thirst_change, cheerfulness_change, radiation_change):
+#         self.hp_change = hp_change
+#         self.hunger_change = hunger_change
+#         self.thirst_change = thirst_change
+#         self.cheerfulness_change = cheerfulness_change
+#         self.radiation_change = radiation_change
+#
+#
+# class Action:
+#     # inventory_set_change is a function, head_armor_change is a function, new_location is a function
+#     def __init__(self, change_function):
+#         self.function_of_changes = change_function
+#
 
-    def __init__(self, hp_change, hunger_change, thirst_change, cheerfulness_change, radiation_change):
-        self.hp_change = hp_change
-        self.hunger_change = hunger_change
-        self.thirst_change = thirst_change
-        self.cheerfulness_change = cheerfulness_change
-        self.radiation_change = radiation_change
-
-
-class Action:
-    # inventory_set_change is a function, head_armor_change is a function, new_location is a function
-    def __init__(self, new_location, main_stat_change, inventory_set_change, observation_change, stealthiness_change,
-                 current_weapon_change, head_armor_change, body_armor_change):
-        self.new_location = new_location
-        self.main_stat_change = main_stat_change
-        self.inventory_set_change = inventory_set_change
-        self.observation_change = observation_change
-        self.stealthiness_change = stealthiness_change
-        self.current_weapon_change = current_weapon_change
-        self.head_armor_change = head_armor_change
-        self.body_armor_change = body_armor_change
-
-
-class Event:
-
-    timer, current_main_character, current_location = 0, MainCharacter.MainCharacter("", 0, 0, 0, 0, 0, set(), 0, 0,
-        Loot.WeaponGenerator.generate_knife(), Loot.ArmorGenerator.generate_motorcycle_helmet(),
-        Loot.ArmorGenerator.generate_motorcycle_jacket()), Locations.LocationGenerator.generate_forest()
-
-    def __init__(self, timer, current_main_character, current_location, current_replica):
+class Condition:
+    
+    def __init__(self, timer, current_main_character, current_location, current_mob, list_of_replicas):
         self.timer = timer
         self.current_main_character = current_main_character
         self.current_location = current_location
-        self.current_replica = current_replica
+        self.current_mob = current_mob
+        self.list_of_replicas = list_of_replicas
 
-    def fight_mob(self, mob):
-        mob_real_damage = mob.damage - self.current_main_character.head_armor - self.current_main_character.body_armor
-        hero_real_damage = self.current_main_character.current_weapon.damage - mob.armor
-        self.current_main_character.hunger -= 5
-        self.current_main_character.thirst -= 5
-        self.current_main_character.cheerfulness -= 5
-        if hero_real_damage <= 0:
-            self.current_main_character.hp = 0
-            return
-        times = ((mob.hp - 1) // hero_real_damage) + 1
-        self.current_main_character.body_armor.durability -= times
-        self.current_main_character.head_armor.durability -= times
-        self.current_main_character.current_weapon.durability -= times
-        if self.current_main_character.body_armor.durability <= 0:
-            self.current_main_character.inventory_set.remove(self.current_main_character.body_armor)
-            self.current_main_character.body_armor = Loot.ArmorGenerator.generate_empty_body()
-            print("Body armor is over")
-        if self.current_main_character.head_armor.durability <= 0:
-            self.current_main_character.inventory_set.remove(self.current_main_character.head_armor)
-            self.current_main_character.head_armor = Loot.ArmorGenerator.generate_empty_head()
-            print("Head armor is over")
-        if self.current_main_character.current_weapon.durability <= 0:
-            self.current_main_character.inventory_set.remove(self.current_main_character.current_weapon)
-            self.current_main_character.current_weapon = Loot.WeaponGenerator.generate_empty_weapon()
-            print("Weapon armor is over")
-        if mob_real_damage < 0:
-            return
-        self.current_main_character.hp -= times * mob_real_damage
+
+class Event:
+    condition = Condition(0, MainCharacter.MainCharacter("", 0, 0, 0, 0, 0,
+        set(), 0, 0,
+        Loot.WeaponGenerator.generate_knife(),
+        Loot.ArmorGenerator.generate_motorcycle_helmet(),
+        Loot.ArmorGenerator.generate_motorcycle_jacket()), Locations.LocationGenerator.generate_forest(), \
+        Mobs.MobGenerator.generate_empty(), list())
+
+    def __init__(self, timer, current_main_character, current_location, current_mob, list_of_replicas):
+        self.condition = Condition(timer, current_main_character, current_location, current_mob, list_of_replicas)
 
     def action(self):
-    # no need if self.current_replica.check_if_applicable due to we've already checked it before in the end of the previous action
-        print(self.current_replica.text)
-        if self.current_replica.text_of_choice_before == "Explore current location":
-            self.current_main_character.inventory_set.update(self.current_location.explore())
-            self.current_main_character.hunger -= 5
-            self.current_main_character.thirst -= 5
-            self.current_main_character.cheerfulness -= 5
-            self.current_main_character.radiation += self.current_location.radiation_per_time
-            self.timer += 1
-            list_of_replicas_possible = list()
-            for replica in self.current_replica.list_of_replicas_next:
-                if replica.check_if_applicable():
-                    print(replica.text)
-                    list_of_replicas_possible.append(replica)
-            index = int(input()) - 1
-            self.current_replica = list_of_replicas_possible[index]
-            return self.action()
-        act = self.current_replica.main_action
-
+        # no need if current_replica.check_if_applicable due to we've already checked
+        # it before in the end of the previous action
+        current_replica = self.condition.list_of_replicas[-1]
+        main_character_temp = self.condition.current_main_character
+        print(current_replica.text)
+        # comment all below due to we've got the function in replica
+        # if current_replica.text_of_choice_before == "Explore current location":
+        #     main_character_temp.inventory_set.update(self.condition.current_location.explore())
+        #     main_character_temp.hunger -= 5
+        #     main_character_temp.thirst -= 5
+        #     main_character_temp.cheerfulness -= 5
+        #     main_character_temp.radiation += self.condition.current_location.radiation_per_time
+        #     self.condition.timer += 1
+        #     list_of_replicas_possible = list()
+        #     for replica in current_replica.list_of_replicas_next:
+        #         if replica.check_if_applicable():
+        #             print(replica.text)
+        #             list_of_replicas_possible.append(replica)
+        #     index = int(input()) - 1
+        #     current_replica = list_of_replicas_possible[index]
+        #     return self.action()
+        # self, text_of_choice_before, text, check_if_applicable, main_action, list_of_answers,
+        #                  map_answer_hash_replica
+        current_replica.main_action(self.condition)
+        list_of_replicas_possible = list()
+        print("Options: ")
+        counter = 0
+        for answer in current_replica.list_of_answers:
+            if current_replica.map_answer_hash_replica[hash(answer)].check_if_applicable(self.condition):
+                counter += 1
+                print(str(counter) + ": " + answer)
+                list_of_replicas_possible.append(current_replica.map_answer_hash_replica[hash(answer)])
+        print("\nChoose the option by it's number in list beginning from 1")
+        index = int(input()) - 1
+        self.condition.list_of_replicas.append(list_of_replicas_possible[index])
+        return self.action()
         # TODO: loot from monsters, location, explore location
-        self.timer += 1
     # each possibility has it's action in dict and variants of next possibilities
-
