9acef762c98a2aa781c218b9023f9ba79a2bb1be
Blinov-Ilya <blinov.is@phystech.edu>
2022-04-17 23:59:08 +0300

    Adding command line ardument possible

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import FileHandler
 
 
 # last_replica = EventLoop.Replica("")
-file_handler = FileHandler.FileHandler("/home/ilya/PycharmProjects/QuestProject/trier.py")
+path = sys.argv[1]
+file_handler = FileHandler.FileHandler(path)
 main_event = EventLoop.Event(file_handler.timer, file_handler.current_main_character, file_handler.current_location,
                              file_handler.current_mob, file_handler.replica_to_begin, file_handler.database)
 main_event.action()
