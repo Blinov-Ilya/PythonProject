917e4ab7b3aada9950c3527b1b2d51ca1540ffad
Blinov-Ilya <blinov.is@phystech.edu>
2022-05-11 23:53:31 +0300

    Changing main

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 import FileHandler
 import app
 
-app.app.run("0.0.0.0", 5000)
-path = sys.argv[1]
-file_handler = FileHandler.FileHandler(path)
-main_event = EventLoop.Event(file_handler.timer, file_handler.current_main_character, file_handler.current_location,
-                             file_handler.current_mob, file_handler.replica_to_begin, file_handler.database)
-main_event.action()
+
+def main():
+    app.app.run("0.0.0.0", 5000)
+
+if __name__ == '__main__':
+    main()
 
